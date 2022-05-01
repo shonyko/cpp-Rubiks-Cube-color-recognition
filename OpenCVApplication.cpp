ade429e0cb9c59f09ad36fa3c743c2cc01b60b13
Alex Kovacs <alexkovacs@rocketmail.com>
2022-05-01 22:59:32 +0300

    Add project files.

---
+++ OpenCVApplication.cpp
@@ -0,0 +1,11 @@
+// OpenCVApplication.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "common.h"
+
+
+int main() {
+	printf("Hello World!");
+	return 0;
+}
